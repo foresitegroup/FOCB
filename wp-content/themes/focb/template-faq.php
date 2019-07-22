459fe2b658db1f513b6a7cce0fc7307a840a0651
foresitegroup <mark@foresitegrp.com>
2019-07-22 17:03:11 -0500

    2019-07-22

---
+++ wp-content/themes/focb/template-faq.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: FAQ */
+
+get_header();
+?>
+
+<div id="page-header">
+  <?php the_title('<h1 class="site-width">', '</h1>'); ?>
+</div>
+
+<div id="faq">
+  <div class="site-width">
+    <?php
+    while(have_posts()) : the_post();
+      the_content();
+    endwhile;
+    ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
