70f5896f2d5fa4c5dcd9662dabd6d42a9502dd80
foresitegroup <mark@foresitegrp.com>
2019-09-12 13:52:43 -0500

    Add meta description and keywords

--- wp-content/themes/focb/header.php
+++ wp-content/themes/focb/header.php
@@ -25,6 +25,9 @@
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
+  <meta name="description" content="The Friends of the Cedarburg Bog support stewardship and appreciation of the biologically diverse Bog through land management, preservation, research and education.">
+  <meta name="keywords" content="Cedarburg bog, Cedarburg, bog, wildlife, wisconsin wildlife, preservation, nature walks, wetland, southern wisconsin, swamp, marsh, bog, southeast wisconsin, nature, nonprofit, educational programs, wildlife research">
+
   <?php wp_head(); ?>
 
   <script type="text/javascript">
@@ -38,14 +41,14 @@
         if (!$('#toggle-menu').is(":checked")) $("#main-header").removeClass("mh-scroll");
       }
 
-      $(window).scroll(function() {    
+      $(window).scroll(function() {
 		    if ($(window).scrollTop() >= 105) {
 		      $("#main-header").addClass("mh-scroll");
 		    } else {
 		      if (!$('#toggle-menu').is(":checked")) $("#main-header").removeClass("mh-scroll");
 		    }
 			});
-      
+
       $('#toggle-menu').change(function() {
         if ($(this).is(":checked")) {
           $("#main-header").addClass("mh-scroll");
@@ -65,7 +68,7 @@
   		<a href="<?php echo home_url(); ?>" id="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Friends of the Cedarburg Bog"></a>
 
 	  	<?php wp_nav_menu(array('theme_location'=>'top-menu','container'=>'nav')); ?>
-	  	
+
 	  	<input type="checkbox" id="toggle-menu" role="button">
       <label for="toggle-menu"><div></div></label>
 	  	<?php wp_nav_menu(array('theme_location'=>'primary-menu','container'=>'nav')); ?>
